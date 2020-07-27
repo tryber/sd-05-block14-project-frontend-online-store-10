c8baface96d249906e5ba783e2fe25c9050cd955
fabiano <fasnaweb2020@gmail.com>
2020-07-27 17:32:29 -0300

    Layout sem css card-04

---
+++ src/components/Pesquisa/Pesquisa.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Pesquisa.css';
+
+class Pesquisa extends React.Component {
+  render() {
+    return (
+      <input type="text" className="pesquisa-input"/>
+    );
+  }
+}
+
+export default Pesquisa;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import Pesquisa from '../../components/Pesquisa';
-
+import Pesquisa from '../../components/Pesquisa/Pesquisa';
+import './Home.css'
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <Pesquisa />
+      <div className="container">
+       <aside className="categoria">
+        <h3>Categorias</h3>
+        <ul>
+          <li>1</li>
+        </ul>
+      </aside>
+      <div className="conteudo">
+       <Pesquisa />
+       <h3 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+       </h3>
       </div>
+      <div><h1>BT</h1></div>
+    </div>        
     );
   }
 }
