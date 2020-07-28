9bfcc42df78fedf82a1cedabf7b2242f9ab4bcc7
pauloricardoz <trybe.przt@gmail.com>
2020-07-28 16:00:29 -0300

    delete

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
 export async function getCategories() {
   // implement here
+  fetch('https://api.mercadolibre.com/sites/MLB/categories')
+  .then(response => response.json())
+  
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
