85c931b3416a3be27618e3353291fbfbf7f5b520
Eder Sena <eder.sena@live.com>
2020-07-28 20:05:51 -0300

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import Pesquisa from '../../components/Pesquisa/Pesquisa';
 import './Home.css'
+
 class Home extends React.Component {
   render() {
     return (
       <div className="container">
-       <aside className="categoria">
-        <h3>Categorias</h3>
-        <ul>
-          <li>1</li>
-        </ul>
-      </aside>
+        <aside className="categoria">
+          <h3>Categorias</h3>
+          <ul>
+            <li>1</li>
+          </ul>
+                                                      </aside>
       <div className="conteudo">
        <Pesquisa />
        <h3 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
+         Digite algum termo de pesquisa ou escolha uma categoria.
        </h3>
       </div>
       <div><h1>BT</h1></div>
