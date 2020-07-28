c5ef547449bc44ff06297be156e53265fe10e156
fabiano <fasnaweb2020@gmail.com>
2020-07-28 16:36:13 -0300

    categorias com radio

--- src/components/Categorias.jsx
+++ src/components/Categorias.jsx
@@ -7,10 +7,10 @@ import React from 'react';
         const { dadosApi} = this.props
         return(
          <div>
-           <h3>Categorias</h3>
-            <ul>
-        {dadosApi.map(elemento => <li key={elemento.id}>{elemento.name}</li>)}
-            </ul>
+           <label>Categorias
+             {dadosApi.map(elemento => <div><label>
+                 <input type="radio" name="categoria" key={elemento.id} value={elemento.name} />{elemento.name} </label></div>)}
+            </label>
         </div>
             )
         }
