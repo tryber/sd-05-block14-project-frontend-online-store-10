ce5790c210300db6adb28568b8481e92d0bdac1d
Eder Sena <eder.sena@live.com>
2020-07-28 23:12:53 -0300

    API

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,12 @@ export async function getCategories() {
   return fetch('https://api.mercadolibre.com/sites/MLB/categories')
     .then((categorias) => categorias.json())
     .then((data) => data)
-    .catch((erro) => erro);
+    .catch((error) => error);
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // implement here
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+    .then((category) => category.json())
+    .then((data) => data)
+    .catch((error) => error);
 }
