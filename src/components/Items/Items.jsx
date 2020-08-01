6c19d29866c188749ba27af90a6c62d499e14331
Cass007 <thecastieel07@gmail.com>
2020-08-01 17:07:10 -0300

    detalhe do produto ok

--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -13,11 +13,19 @@ const Item = (props) => {
       {items.map((item) => (
         <div data-testid="product" key={item.id} className="item">
           <p>{item.title}</p>
-          <Link data-testid="product-detail-link" to={{ pathname: `/${item.id}`, item }}>
+          <Link
+            data-testid="product-detail-link"
+            to={{ pathname: `/${item.id}`, item }}
+          >
             <img src={item.thumbnail} alt="item" />
           </Link>
           <p>{`R$ ${item.price.toFixed(2)}`}</p>
-          <AddToCart dataTestid="product-add-to-cart" item={item} quantity={1} plusQuant={false} />
+          <AddToCart
+            dataTestid="product-add-to-cart"
+            item={item}
+            quantity={1}
+            plusQuant={false}
+          />
         </div>
       ))}
     </div>
